52e0bfcd80ba36c34cdf282e1aa99a337cfe769a
Shambhav <shambhav.acharya@foneypay.com>
2025-05-12 22:45:07 +0545

    Add Spring Boot dependencies and initialize project structure

---
+++ src/main/java/com/freelance/invoice_manager/InvoiceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.freelance.invoice_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManagerApplication.class, args);
+	}
+
+}
